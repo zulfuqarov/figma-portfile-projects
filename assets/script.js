b8bcd46ef348122cb24a222c94f67fc0478a4285
zulfuqarov <zulfuqarov.nebi@gmail.com>
2023-04-06 07:11:43 -0700

    lahiye hazirdir

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function show(val){
+    if(val==1){
+        document.getElementById('main').style.display='none';
+    }
+    if(val==2){
+        document.getElementById('main').style.display='block';
+    }
+}
